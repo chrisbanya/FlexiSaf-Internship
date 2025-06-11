8e5412b04ef7c6dcbfedc414aedb2af19472da0a
Adabanya Christian <adabanyachristian21@gmail.com>
2025-06-11 22:23:38 +0100

    removed folder

--- RESTfulApi/src/components/Filter.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Filter = () => {
-  return (
-    <div className="mb-4">
-      <input type="text" placeholder="search" className="p-2 w-full md:w-72 border rounded" />
-    </div>
-  );
-}
